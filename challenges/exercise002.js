c94f4e30abf419ef4338fbe11334778147527c8b
SukirtiSaxena <91497529+SukirtiSaxena@users.noreply.github.com>
2021-09-29 10:45:05 +0100

    Implemented exercise002.js
    Implemented exercise002.js

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,48 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
   // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  if (person.city === "Manchester")
+    return true;
+  else
+    return false;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  let peopleInOneBus = 40;
+  let NumberOfBuses = 1;
+
+  if (people > 40)
+    NumberOfBuses = people/peopleInOneBus;
+  return Math.ceil(NumberOfBuses);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  let farmAnimal = "sheep";
+  let count = 0;
+  for (let i = 0; i < arr.length ; i++) {
+    if (arr[i] === farmAnimal)
+    count +=1;
+  }
+  return count;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  if (person.address.postCode.charAt(0) === "M" && person.address.city === "Manchester" )
+    return true;
+  else
+    return false;
 }
 
 module.exports = {

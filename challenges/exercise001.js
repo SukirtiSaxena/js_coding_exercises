5f070fade01b4dfa674a903aa043c9727208122e
SukirtiSaxena <91497529+SukirtiSaxena@users.noreply.github.com>
2021-10-06 10:16:02 +0100

    exercise001.js
    Implemented refracting in exercise001.js

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,118 +1,81 @@
-// to test   > npm test test/exercise001.test.js
+  // to test   > npm test test/exercise001.test.js
 
-function capitalize(word) {
-  if (word === undefined) throw new Error("word is required");
-  // Add your code here!
-  let capitalWord = word.charAt(0).toUpperCase() + word.slice(1);
-  return capitalWord;
-}
+  function capitalize(word) {
+    if (word === undefined) throw new Error("word is required");
 
-function generateInitials(firstName, lastName) {
-  if (firstName === undefined) throw new Error("firstName is required");
-  if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
-  let initials = firstName.charAt(0) +"."+ lastName.charAt(0); 
-  return initials;
-}
-
-function addVAT(originalPrice, vatRate) {
-  if (originalPrice === undefined) throw new Error("originalPrice is requied");
-  if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
-  let finalPrice = originalPrice + (originalPrice * vatRate)/100;
-  finalPrice = Math.round(finalPrice * 100) / 100;
-  return finalPrice;
-}
-
-function getSalePrice(originalPrice, reduction) {
-  if (originalPrice === undefined) throw new Error("originalPrice is required");
-  if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
-  let finalPrice = originalPrice - (originalPrice * reduction)/100;
-  finalPrice = Math.round(finalPrice * 100) / 100;
-  return finalPrice;
-}
-
-function getMiddleCharacter(str) {
-  if (str === undefined) throw new Error("str is required");
-  // Add your code here!
-  let middleChar = 0;
-  if ((str.length % 2) == 0 )
-     middleChar = str.charAt((str.length/2) -1)+ str.charAt(str.length/2);
-  else 
-    middleChar = str.charAt(str.length/2);
-  return middleChar;
-}
-
-function reverseWord(word) {
-  if (word === undefined) throw new Error("word is required");
-  // Add your code here!
-  var revStr = "";
-  for (var i = word.length - 1; i >= 0; i--) 
-      revStr += word[i];
-  return revStr;
-}
-
-function reverseAllWords(words) {
-  if (words === undefined) throw new Error("words is required");
-  // Add your code here!
-  reverseWords = [];
-  for (let i = 0; i< words.length; i++) {
-       let revString = reverseWord(words[i]);
-       reverseWords.push(revString);
+    return word.charAt(0).toUpperCase() + word.slice(1);
   }
-  return reverseWords;
 
-}
+  function generateInitials(firstName, lastName) {
+    if (firstName === undefined) throw new Error("firstName is required");
+    if (lastName === undefined) throw new Error("lastName is required");
 
-function countLinuxUsers(users) {
-  if (users === undefined) throw new Error("users is required");
-  // Add your code here!
-  let count = 0;
-  for (let i = 0; i < users.length ; i++) {
-	  if (users[i].type === "Linux") {
-		  count += 1;
-	  }
+    return firstName.charAt(0) +"."+ lastName.charAt(0);
   }
-  return count;
-	
-}
 
-function getMeanScore(scores) {
-  if (scores === undefined) throw new Error("scores is required");
-  // Add your code here!
-  let total = 0;
-  let mean = 0;
-  for(let i = 0; i < scores.length; i++) {
-    total += scores[i];	
-}
-  mean = total / scores.length;
-  mean = Math.round(mean * 100) / 100;
-  return mean; 
-}
+  function addVAT(originalPrice, vatRate) {
+    if (originalPrice === undefined) throw new Error("originalPrice is requied");
+    if (vatRate === undefined) throw new Error("vatRate is required");
 
-function simpleFizzBuzz(n) {
-  if (n === undefined) throw new Error("n is required");
-  // Add your code here!
-  let output = '';
-  if ((n % 3) == 0) 
-     output += "fizz";
-  if ((n % 5 ) == 0)		
-     output += "buzz"
-  if ((n % 3) !== 0 && (n % 5) !== 0)
-     output = n;
-    return output;
-}
+    return Math.round((originalPrice + (originalPrice * vatRate)/100) * 100) / 100;
+  }
 
-module.exports = {
-  capitalize,
-  generateInitials,
-  addVAT,
-  getSalePrice,
-  getMiddleCharacter,
-  reverseWord,
-  reverseAllWords,
-  countLinuxUsers,
-  getMeanScore,
-  simpleFizzBuzz
-};
+  function getSalePrice(originalPrice, reduction) {
+    if (originalPrice === undefined) throw new Error("originalPrice is required");
+    if (reduction === undefined) throw new Error("reduction is required");
+  
+    return Math.round((originalPrice - (originalPrice * reduction)/100) * 100) / 100;
+  }
+
+  function getMiddleCharacter(str) {
+    if (str === undefined) throw new Error("str is required");
+    return ((str.length % 2) == 0 ? str.charAt((str.length/2) -1)+ str.charAt(str.length/2) : str.charAt(str.length/2));
+  }
+
+  function reverseWord(word) {
+    if (word === undefined) throw new Error("word is required");
+    let revStr = "";
+    for (let i = word.length - 1; i >= 0;  revStr += word[i--]) ;
+    return revStr;
+  }
+
+  function reverseAllWords(words) {
+    if (words === undefined) throw new Error("words is required");
+    let reverseWords = [];
+    for (let i = 0; i< words.length; reverseWords.push(reverseWord(words[i++]))) ;
+
+    return reverseWords;
+
+  }
+
+    function countLinuxUsers(users) {
+      if (users === undefined) throw new Error("users is required");
+      let count = 0;
+      for(let i = 0 ; i<users.length;count+=(Number(users[i++].type === "Linux")));
+      return count;
+  }
+
+  function getMeanScore(scores) {
+    if (scores === undefined) throw new Error("scores is required");
+    let total = 0;
+    for(let i = 0; i < scores.length; total += scores[i++]);
+    return Math.round((total / scores.length) * 100) / 100; 
+  }
+
+  function simpleFizzBuzz(n) {
+    if (n === undefined) throw new Error("n is required");
+    return ((n % 3) !== 0 && (n % 5) ?  n : (n % 3 ? "" : "fizz") + (n % 5 ? "" : "buzz"));
+  }
+
+  module.exports = {
+    capitalize,
+    generateInitials,
+    addVAT,
+    getSalePrice,
+    getMiddleCharacter,
+    reverseWord,
+    reverseAllWords,
+    countLinuxUsers,
+    getMeanScore,
+    simpleFizzBuzz
+  };

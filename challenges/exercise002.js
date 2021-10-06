3060c4b5a351eb8e910331c89ec550ed84fb43d4
SukirtiSaxena <91497529+SukirtiSaxena@users.noreply.github.com>
2021-10-06 10:29:03 +0100

    exercise002.js
    Implemented refracting in exercise002.js

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,54 +1,36 @@
-function getFillings(sandwich) {
-  if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
-  return sandwich.fillings;
-}
-
-function isFromManchester(person) {
-  if (person === undefined) throw new Error("person is required");
-  // Your code here!
-  if (person.city === "Manchester")
-    return true;
-  else
-    return false;
-}
-
-function getBusNumbers(people) {
-  if (people === undefined) throw new Error("people is required");
-  // Your code here!
-  let peopleInOneBus = 40;
-  let NumberOfBuses = 1;
-
-  if (people > 40)
-    NumberOfBuses = people/peopleInOneBus;
-  return Math.ceil(NumberOfBuses);
-}
-
-function countSheep(arr) {
-  if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
-  let farmAnimal = "sheep";
-  let count = 0;
-  for (let i = 0; i < arr.length ; i++) {
-    if (arr[i] === farmAnimal)
-    count +=1;
+  function getFillings(sandwich) {
+    if (sandwich === undefined) throw new Error("ingredients is required");
+    return sandwich.fillings;
   }
-  return count;
-}
 
-function hasMPostCode(person) {
-  if (person === undefined) throw new Error("person is required");
-  // Your code here!
-  if (person.address.postCode.charAt(0) === "M" && person.address.city === "Manchester" )
-    return true;
-  else
-    return false;
-}
+  function isFromManchester(person) {
+    if (person === undefined) throw new Error("person is required");
+    return (person.city === "Manchester");
+  }
 
-module.exports = {
-  getFillings,
-  isFromManchester,
-  countSheep,
-  getBusNumbers,
-  hasMPostCode
-};
+  function getBusNumbers(people) {
+    if (people === undefined) throw new Error("people is required");
+    let peopleInOneBus = 40;
+    return Math.ceil(people/peopleInOneBus);
+  }
+
+  function countSheep(arr) {
+    if (arr === undefined) throw new Error("arr is required");
+    let farmAnimal = "sheep";
+    let count = 0;
+    for (let i = 0; i < arr.length ; count += Number((arr[i++] === farmAnimal)));
+    return count;
+  }
+
+  function hasMPostCode(person) {
+    if (person === undefined) throw new Error("person is required");
+    return person.address.postCode.charAt(0) === "M" && person.address.city === "Manchester" ;
+  }
+
+  module.exports = {
+    getFillings,
+    isFromManchester,
+    countSheep,
+    getBusNumbers,
+    hasMPostCode
+  };

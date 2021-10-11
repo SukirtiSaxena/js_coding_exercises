c2812de64c8da4b5de3c64036e4471c48418931f
SukirtiSaxena <91497529+SukirtiSaxena@users.noreply.github.com>
2021-10-11 18:43:10 +0100

    Exercise006.js
    Implemented refactoring in exercise006.js

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -1,127 +1,116 @@
-/**
- * This function will receive an array of numbers and should return the sum
- * of any numbers which are a multiple of 3 or 5
- * @param {Array} arr
- * @returns {Number}
- */
-const sumMultiples = arr => {
-  if (arr === undefined) throw new Error("arr is required");
-  let sumOfNumbers = 0;
-  for (let i=0;i<arr.length;i++)
-	  if (arr[i] % 3 === 0 || arr[i] % 5 === 0)
-		  sumOfNumbers += arr[i];
-  return sumOfNumbers;
-};
+  /**
+   * This function will receive an array of numbers and should return the sum
+   * of any numbers which are a multiple of 3 or 5
+   * @param {Array} arr
+   * @returns {Number}
+   */
+  const sumMultiples = arr => {
+    if (arr === undefined) throw new Error("arr is required");
+    let sumOfNumbers = 0;
+    arr.forEach(num => {
+      if(num % 3 === 0 || num % 5 === 0)
+        sumOfNumbers +=num
+    });
+    return sumOfNumbers;
+  };
 
-/**
- * This function will receive a string of characters and should return true/false depending on whether it is a valid DNA string. A valid DNA string may contain characters C, G, T or A only.
- * @param {String} str
- * @returns {Boolean}
- */
-const isValidDNA = str => {
-  if (str === undefined) throw new Error("str is required");
-  let validDNA  = ["C", "G", "T", "A"];
-  for(let i= 0; i < str.length; i++){
-    if(!validDNA.includes(str[i])){
-      return false;
-     }
-   }
-  return true;
-};
-
-/**
- * This function will receive a valid DNA string (see above) and should return a string of the complementary base pairs. In DNA, T always pairs with A, and C always pairs with G. So a string of "ACTG" would have a complementary DNA string of "TGAC".
- * @param {String} str
- * @returns {String}
- */
-const getComplementaryDNA = str => {
-  if (str === undefined) throw new Error("str is required");
-  let pairObj = {
-    "A": "T",
-    "T": "A",
-    "G": "C",
-    "C": "G"
-    };
-
-  if (isValidDNA(str)) {
-     str = str.replace(/A|G|C|T/gi, function(matched){
-           return pairObj[matched];
-           });
-   return str; 
-  }
-};
-
-/**
- * This function should receive a number and return true/false depending on whether it is a prime number or not. A prime number is a number that can only be divided evenly by 1 and itself (for example, 7)
- * @param {Number} n
- * @returns {Boolean}
- */
-const isItPrime = n => {
-  if (n === undefined) throw new Error("n is required");
-  for(let i = 2; i < n; i++){
-      if(n % i=== 0)
+  /**
+   * This function will receive a string of characters and should return true/false depending on whether it is a valid DNA string. A valid DNA string may contain characters C, G, T or A only.
+   * @param {String} str
+   * @returns {Boolean}
+   */
+  const isValidDNA = str => {
+    if (str === undefined) throw new Error("str is required");
+    let validDNA  = ["C", "G", "T", "A"];
+    for(let i= 0; i < str.length; i++){
+      if(!validDNA.includes(str[i])){
         return false;
-  }
-  return true;
-};
+      }
+    }
+    return true;
+  };
 
-/**
- * This function should receive a number and return an array of n arrays, each filled with n items. The parameter "fill" should be used as the filler of the arrays. For example, given parameters 3 and "foo" the resulting matrix should be:
- * [
- *   ["foo", "foo", "foo"],
- *   ["foo", "foo", "foo"],
- *   ["foo", "foo", "foo"]
- * ]
- * @param {Number} n
- * @param {Any} fill
- * @returns {Array}
- */
-const createMatrix = (n, fill) => {
-  if (n === undefined) throw new Error("n is required");
-  if (fill === undefined) throw new Error("fill is required");
-  let newArr = [];
-  for (let i = 0; i < n;i++){
-      newArr[i] = new Array(n);
-      for(let j = 0; j<n ;j++) {
-	       newArr[i][j] = fill;
-	    }
-  }
-   return newArr;
-};
+  /**
+   * This function will receive a valid DNA string (see above) and should return a string of the complementary base pairs. In DNA, T always pairs with A, and C always pairs with G. So a string of "ACTG" would have a complementary DNA string of "TGAC".
+   * @param {String} str
+   * @returns {String}
+   */
+  const getComplementaryDNA = str => {
+    if (str === undefined) throw new Error("str is required");
+    let pairObj = {
+      "A": "T",
+      "T": "A",
+      "G": "C",
+      "C": "G"
+      };
 
-/**
- * This function takes an array of staff objects in the format:
- * [
- *  { name: "Sally", rota: ["Monday", "Tuesday", "Friday"] },
- *  { name: "Pedro", rota: ["Saturday", "Sunday", "Tuesday", "Wednesday"] },
- *  ...etc
- * ]
- * and a day of the week. For the café to run successfully, at least 3 staff members are required per day. The function should return true/false depending on whether there are enough staff scheduled for the given day.
- * @param {Array} staff
- * @param {String} day
- * @returns {Boolean}
- */
-const areWeCovered = (staff, day) => {
-  if (staff === undefined) throw new Error("staff is required");
-  if (day === undefined) throw new Error("day is required");
-  let staffCount = 0;
-  for (let i=0; i<staff.length;i++){
-	for (let j=0;j<staff[i].rota.length;j++) {
-	    if (staff[i].rota[j] === day)
-		     staffCount++;	
-      }	
-  }
-  if (staffCount ===3)
-	   return true;
-  else
-	   return false;
-};
+    if (isValidDNA(str)) {
+      str = str.replace(/A|G|C|T/gi, function(matched){
+            return pairObj[matched];
+            });
+    return str; 
+    }
+  };
 
-module.exports = {
-  sumMultiples,
-  isValidDNA,
-  getComplementaryDNA,
-  isItPrime,
-  createMatrix,
-  areWeCovered
-};
+  /**
+   * This function should receive a number and return true/false depending on whether it is a prime number or not. A prime number is a number that can only be divided evenly by 1 and itself (for example, 7)
+   * @param {Number} n
+   * @returns {Boolean}
+   */
+  const isItPrime = n => {
+    if (n === undefined) throw new Error("n is required");
+    for(let i = 2; i < n; i++){
+        if(n % i=== 0)
+          return false;
+    }
+    return true;
+  };
+
+  /**
+   * This function should receive a number and return an array of n arrays, each filled with n items. The parameter "fill" should be used as the filler of the arrays. For example, given parameters 3 and "foo" the resulting matrix should be:
+   * [
+   *   ["foo", "foo", "foo"],
+   *   ["foo", "foo", "foo"],
+   *   ["foo", "foo", "foo"]
+   * ]
+   * @param {Number} n
+   * @param {Any} fill
+   * @returns {Array}
+   */
+  const createMatrix = (n, fill) => {
+    if (n === undefined) throw new Error("n is required");
+    if (fill === undefined) throw new Error("fill is required");
+    return Array(n).fill().map(() => Array(n).fill(fill));
+  };
+
+  /**
+   * This function takes an array of staff objects in the format:
+   * [
+   *  { name: "Sally", rota: ["Monday", "Tuesday", "Friday"] },
+   *  { name: "Pedro", rota: ["Saturday", "Sunday", "Tuesday", "Wednesday"] },
+   *  ...etc
+   * ]
+   * and a day of the week. For the café to run successfully, at least 3 staff members are required per day. The function should return true/false depending on whether there are enough staff scheduled for the given day.
+   * @param {Array} staff
+   * @param {String} day
+   * @returns {Boolean}
+   */
+  const areWeCovered = (staff, day) => {
+    if (staff === undefined) throw new Error("staff is required");
+    if (day === undefined) throw new Error("day is required");
+    let staffCount = 0;
+    staff.forEach(totalStaff => { 
+      if (totalStaff.rota.includes(day))
+        staffCount++; 
+    });
+    return (staffCount === 3 ? true : false);
+  };
+
+  module.exports = {
+    sumMultiples,
+    isValidDNA,
+    getComplementaryDNA,
+    isItPrime,
+    createMatrix,
+    areWeCovered
+  };
